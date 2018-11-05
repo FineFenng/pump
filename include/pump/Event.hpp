ce2b0c4afb48f4abaae91cd631e68545befab4f7
fengchao <fengchao@mail.jj.cn>
2018-11-05 18:00:49 +0800

    coding

--- include/pump/Event.hpp
+++ include/pump/Event.hpp
@@ -2,6 +2,7 @@
 #define PUMP_EVENT_H
 
 #include <map>
+#include <functional>
 #include <type_traits>
 #include <vector>
 
@@ -9,20 +10,39 @@
 
 namespace pump {
 
+struct HandlerId {
+public:
+	HandlerId(long id)
+		: id_(id) {
+	}
+
+	long id_;
+};
+
+inline bool operator==(const HandlerId& lhs, const HandlerId& rhs) {
+	return lhs.id_ == rhs.id_;
+}
+
+inline bool operator!=(const HandlerId& lhs, const HandlerId& rhs) {
+	return !(lhs == rhs);
+}
+
+
 static std::atomic<long> s_event_handler_id(0);
 
 class EventHandlerBase : public pump::RefBase {
 public:
 	EventHandlerBase()
-		: id_(s_event_handler_id.fetch_add(1)) {
+		: handler_id_(s_event_handler_id.fetch_add(1)) {
 	}
 
-	long get_id() const {
-		return id_;
+	long get_handler_id() const {
+		return handler_id_.id_;
 	}
 
 private:
-	long id_;
+
+	struct HandlerId handler_id_;
 
 };
 
@@ -49,6 +69,7 @@ PMRP<EventHandler<Callable>> MakeEventHandler(Callable&& callee) {
 }
 
 class EventTrigger {
+PUMP_DECLARE_DELETE_COPYABLE_AND_MOVABLE(EventTrigger)
 public:
 	typedef std::vector<PMRP<EventHandlerBase>> HandlerVector;
 	typedef std::map<int, HandlerVector> EventMap;
@@ -67,12 +88,12 @@ public:
 		handlers_.insert({id, handlers});
 	}
 
-	void unbind(const long handler_id) {
+	void unbind(const HandlerId& handler_id) {
 		auto it = handlers_.begin();
 		while (it != handlers_.end()) {
 			const auto it_handlers = it->second.begin();
 			while (it_handlers != it->second.end()) {
-				if (handler_id == (*it_handlers)->get_id()) {
+				if (handler_id == (*it_handlers)->get_handler_id()) {
 					it->second.erase(it_handlers);
 				}
 				return;
@@ -81,11 +102,45 @@ public:
 		}
 	}
 
+	template <typename Callable>
+	HandlerId bind(int event_id, Callable&& event_callee) {
+		static_assert(std::is_class<std::remove_reference<Callable>>::value, "Callable must be callable object");
+		const PMRP<EventHandlerBase> EventHandler = MakeEventHandler<Callable>(std::forward<Callable>(event_callee));
+		insert_into_map(event_id, EventHandler);
+		return EventHandler->get_handler_id();
+	}
+
+	template <typename CallableHint, typename Callable,
+			class = typename std::enable_if<std::is_convertible<Callable, CallableHint>::value>::type>
+	HandlerId bind(int event_id, Callable&& event_callee) {
+		static_assert(std::is_class<std::remove_reference<Callable>>::value, "Callable must be callable object");
+		const PMRP<EventHandlerBase> EventHandler = MakeEventHandler<Callable>(std::forward<Callable>(event_callee));
+		insert_into_map(event_id, EventHandler);
+		return EventHandler->get_handler_id();
+	}
+
+	template <typename CallableHint, typename Function, typename ...Args>
+	HandlerId bind(int event_id, Function&& func, Args&&... args) {
+		const PMRP<EventHandlerBase> EventHandler =
+			MakeEventHandler<CallableHint>(std::bind(std::forward<Function>(func), std::forward<Args>(args)...));
+		insert_into_map(event_id, EventHandler);
+		return EventHandler->get_handler_id();
+	}
+	template<typename CallableHint, typename ...Args>
+	void invoke(int event_id, Args&&... args) {
+		auto it = handlers_.find(event_id);
+		if(it!= handlers_.end()) {
+			auto it_handler = it->second.begin();
+			while (it_handler != it->second.end()) {
+				PMRP<EventHandler<CallableHint>> callee_handler = *(it_handler++);
+				callee_handler->call(std::forward<Args>(args)...);
+			}
+		}
+	}
 
 private:
 	EventMap handlers_;
 };
-
 
 }
 #endif
